8c6e6a82f5142fea171fd56806b9b398d9f7291b
P S SAI RATAN <113715546+SaiRatan1@users.noreply.github.com>
2023-03-11 11:27:19 +0530

    Delete invalid.js

--- server/static/invalid.js
+++
@@ -1,9 +0,0 @@
-// function invalidcred(){
-//     let inval = document.getElementsByClassName('invalid')[0];
-//     // console.log(inval)
-//     // inval.style.color="red";
-//     inval.innerHTML=  "";
-// }
-// invalidcred();
-
-// module.exports = invalidcred;
